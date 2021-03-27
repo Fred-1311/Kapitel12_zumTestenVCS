e27dc3ad95e6420070dcee7e7f555dd001508f7b
Fred <arachbauer@aon.at>
2021-03-27 19:50:42 +0100

    Erstes Commit, Quellcode-Dateien hinzugefügt!

---
+++ Beispiel_12_1.py
@@ -0,0 +1,28 @@
+# Beispiel 12.1
+#
+# Eine einfaches Beispiel zum Experimentieren
+#
+import Auto
+
+
+def main():
+    # Hauptprogramm
+    auto_eins = Auto.Auto("Golf", "Schwarz", 110, 5)
+    auto_zwei = Auto.Auto("Golf", "Grün", 50, 3)
+
+    print("\nDaten von Auto eins:")
+    auto_eins.zeige_daten()
+
+    print("\nDaten von Auto zwei:")
+    auto_zwei.zeige_daten()
+
+    print("Neues Auto erzeugt")
+
+    auto_eins.strecke_fahren(64)
+    auto_zwei.strecke_fahren(128)
+
+    print("Kilometerstand des ersten Autos:", auto_eins.kilometerstand)
+    print("Kilometerstand des zweiten Autos:", auto_zwei.kilometerstand)
+
+
+main()
